4d98b787a3d2885bb98348d2c71c2225097dfdd7
ajit-git-example <ajitchoudhary1241@gmail.com>
2023-08-16 15:38:10 +0530

    Cors() is added for the perticular Url

--- Program.cs
+++ Program.cs
@@ -13,6 +13,9 @@ builder.Services.AddSwaggerGen();
 // Configuration for the Database Application
 builder.Services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSqlConnection")));
 
+//configuration for the Cors
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,6 +24,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Adding the middelware of cors
+app.UseCors(builder=>builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200/"));
 
 app.UseHttpsRedirection();
 
