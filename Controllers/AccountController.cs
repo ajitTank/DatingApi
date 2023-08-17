2711d5bab3dbf5de7c5ffee8a42136b144c6ff99
ajit-git-example <ajitchoudhary1241@gmail.com>
2023-08-17 13:34:44 +0530

    Login function completed new HMACSHA512(hmc.key) -- generate the same hash code

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -25,20 +25,27 @@ namespace DatingApi.Controllers
 
 
         [HttpPost("register")]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ApiResponse)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest , Type = typeof(ApiResponse))]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ApiResponse))]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+// its not capturing the interner server error
+// how to to exception handling ?
 
         public async Task<ActionResult<ApiResponse>> register(RegisterDto registerDto)
         {
            
             try
             {
-                if (await this.UOW.accountRepository.GetByValueAsync(x => x.UserName.ToLower() == registerDto.UserName.ToLower()) != null)
+                //here the saving data depends on the user existence and userExit is async type
+                // how to handel the 
+                var UserExist = await userExistAsync(registerDto.UserName);
+                if ( UserExist!= null)
                 {
-                    ErrorResponse(new List<string>() { "User Name is exist" }, HttpStatusCode.BadRequest);
+                    this.response.httpStatusCode = HttpStatusCode.BadRequest;
+                    this.response.ErrorMessage = new List<string>() { "User Name Already exist" };
                     return this.response;
                 }
+                
 
                 using var hmc = new HMACSHA512();
 
@@ -63,6 +70,51 @@ namespace DatingApi.Controllers
                 
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<ApiResponse>> login(LoginDto loginDto)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    this.response.httpStatusCode = HttpStatusCode.BadRequest;
+                    this.response.ErrorMessage = new List<string>() { ModelState.ToString() };
+                    return this.response;
+                }
+                var userExist = await userExistAsync(loginDto.userName);
+                if ( userExist == null)
+                {
+                    {
+                        ErrorResponse(new List<string>() { "User Name not found" }, HttpStatusCode.NotFound);
+                        return this.response;
+                    }
+                }
+                //here passing the hmc.key to hte HMACSHA512 the ctor generate the same hasCode
+                using var hmc = new HMACSHA512(userExist.PasswordSalt);
+                 var hmcCode =  hmc.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+                
+
+               for(int i = 0; i<hmcCode.Length; i++)
+                {
+                    if (hmcCode[i] != userExist.PasswordHash[i])
+                    {
+
+                        ErrorResponse(new List<string>() { "Password Name not found" }, HttpStatusCode.NotFound);
+                        return this.response;
+                    }
+                        
+                }
+               this.SuccessResponse(HttpStatusCode.OK,userExist.UserName);
+                return this.response;   
+            }
+            catch (Exception ex)
+            {
+
+                ErrorResponse(new List<string>() { ex.ToString() }, HttpStatusCode.InternalServerError);
+                return this.response;
+            }
+        }
+
 
         [NonAction]
         public void SuccessResponse(HttpStatusCode httpStatusCode,Object result)
@@ -77,6 +129,17 @@ namespace DatingApi.Controllers
             this.response.isSuccess = false;
             this.response.ErrorMessage = message;
             this.response.httpStatusCode = httpStatusCode;
+        }
+        [NonAction]
+        public async Task<AppUser> userExistAsync(string userName)
+        {
+           var userExist = await this.UOW.accountRepository.GetByValueAsync(x => x.UserName.ToLower() == userName.ToLower());
+            if (userExist == null)
+            {
+                return null;
+            }
+            return userExist;
+              
         }
     }
 }
---
+++ Model/DTO/LoginDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DatingApi.Model.DTO
+{
+    public class LoginDto
+    {
+        [Required]
+        public string userName { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
